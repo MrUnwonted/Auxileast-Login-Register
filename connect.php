23f12e9a53a3191af98315d7430a86742a1edc17
ARJUN <arjunchandran2k@gmail.com>
2022-06-30 15:07:27 +0530

    V2.0

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+    $firstname = $_POST['firstname']
+    $lastname = $_POST['lastname']
+    $gender = $_POST['gender']
+    $email = $_POST['email']
+    $password = $_POST['password']
+    $DOB = $_POST['DOB']
+
+    $conn = new mysqli('localhost','root','','test');
+    if($conn->connect_error){
+        die('Connection Failed : ' .$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into registration(firstname,lastname,DOB,email,gender,password)
+        values(?,?,?,?,?,?)")
+        $stmt->bind_param("ssisss",$firstname,$lastname,$DOB,$email,$gender,$assword)
+        $stmt->execute();
+        echo "Registration Successfully...."
+        $stmt->close();
+        $conn->close();
+    }
+?>
